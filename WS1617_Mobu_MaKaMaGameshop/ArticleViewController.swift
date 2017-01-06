d9d4dc98ef6ac5ca81e05448d686a25f42166e82
Nathalie Kroj <Nathalie@p20030051a7346701d56aa3935b7bd42e.dip0.t-ipconnect.de>
2017-01-06 21:10:13 +0100

    Detail screen mit anwählbaren Editionen

--- WS1617_Mobu_MaKaMaGameshop/ArticleViewController.swift
+++ WS1617_Mobu_MaKaMaGameshop/ArticleViewController.swift
@@ -27,15 +27,15 @@ class ArticleViewController: UIViewController {
     }
     
     func createGameDataAction(){
-        let game1: Spiel = Spiel(gameImageName: "ark", name: "ARK: Survival Envolved", categorie: "Action", price: 29.99)
-        let game2: Spiel = Spiel(gameImageName: "deadbydaylight", name: "Dead by Daylight", categorie: "Action", price: 14.99)
-        let game3: Spiel = Spiel(gameImageName: "deusex", name: "Deus Ex: Mankind Divided", categorie: "Action", price: 24.99)
-        let game4: Spiel = Spiel(gameImageName: "dishonored2", name: "Dishonored 2", categorie: "Action", price: 24.99)
-        let game5: Spiel = Spiel(gameImageName: "gta5", name: "Grand Theft Auto V", categorie: "Action", price: 19.99)
-        let game6: Spiel = Spiel(gameImageName: "h1z1", name: "H1Z1: King of the Kill", categorie: "Action", price: 19.99)
-        let game7: Spiel = Spiel(gameImageName: "residentevil7", name: "Resident Evil VII", categorie: "Action", price: 34.99)
-        let game8: Spiel = Spiel(gameImageName: "thewalkingdead", name: "The Walking Dead: A new frontier", categorie: "Action", price: 19.99)
-        let game9: Spiel = Spiel(gameImageName: "thewitcher3", name: "The Witcher 3: Wild Hunt", categorie: "Action", price: 14.99)
+        let game1: Spiel = Spiel(gameImageName: "ark", name: "ARK: Survival Envolved", categorie: "Action", price: 29.99, teaserImageName: "bannertentacle")
+        let game2: Spiel = Spiel(gameImageName: "deadbydaylight", name: "Dead by Daylight", categorie: "Action", price: 14.99, teaserImageName: "minecraftheader")
+        let game3: Spiel = Spiel(gameImageName: "deusex", name: "Deus Ex: Mankind Divided", categorie: "Action", price: 24.99, teaserImageName: "minecraftheader")
+        let game4: Spiel = Spiel(gameImageName: "dishonored2", name: "Dishonored 2", categorie: "Action", price: 24.99, teaserImageName: "minecraftheader")
+        let game5: Spiel = Spiel(gameImageName: "gta5", name: "Grand Theft Auto V", categorie: "Action", price: 19.99, teaserImageName: "minecraftheader")
+        let game6: Spiel = Spiel(gameImageName: "h1z1", name: "H1Z1: King of the Kill", categorie: "Action", price: 19.99, teaserImageName: "minecraftheader")
+        let game7: Spiel = Spiel(gameImageName: "residentevil7", name: "Resident Evil VII", categorie: "Action", price: 34.99, teaserImageName: "minecraftheader")
+        let game8: Spiel = Spiel(gameImageName: "thewalkingdead", name: "The Walking Dead: A new frontier", categorie: "Action", price: 19.99, teaserImageName: "minecraftheader")
+        let game9: Spiel = Spiel(gameImageName: "thewitcher3", name: "The Witcher 3: Wild Hunt", categorie: "Action", price: 14.99, teaserImageName: "minecraftheader")
                 // Section: 0 = Action, 1= P&C, 2 = Rpg, 3 = Simulation, 4 = Sports, 5 = Strategie
         
         games.append(game1)
@@ -50,15 +50,15 @@ class ArticleViewController: UIViewController {
     }
     
     func createGameDataPointandClick(){
-        let game1: Spiel = Spiel(gameImageName: "dayofthetentacle", name: "Day of the Tentacle", categorie: "Point and Click", price: 12.99)
-        let game2: Spiel = Spiel(gameImageName: "decay", name: "Decay - The Mare", categorie: "Point and Click", price: 6.99)
-        let game3: Spiel = Spiel(gameImageName: "indianajones", name: "Indiana Jones and ...", categorie: "Point and Click", price: 4.99)
-        let game4: Spiel = Spiel(gameImageName: "loom", name: "Loom", categorie: "Point and Click", price: 5.99)
-        let game5: Spiel = Spiel(gameImageName: "maniacmanson", name: "Maniac Manson", categorie: "Point and Click", price: 999.99)
-        let game6: Spiel = Spiel(gameImageName: "randalsmonday", name: "Randals's Monday", categorie: "Point and Click", price: 7.99)
-        let game7: Spiel = Spiel(gameImageName: "samandmax", name: "Sam & Max: Hit the Road", categorie: "Point and Click", price: 29.99)
-        let game8: Spiel = Spiel(gameImageName: "secretofmonkeyisland", name: "Secret of Monkey Island", categorie: "Point and Click", price: 9.99)
-        let game9: Spiel = Spiel(gameImageName: "zakmckracken", name: "Zak McKracken", categorie: "Point and Click", price: 249.99)
+        let game1: Spiel = Spiel(gameImageName: "dayofthetentacle", name: "Day of the Tentacle", categorie: "Point and Click", price: 12.99, teaserImageName: "minecraftheader")
+        let game2: Spiel = Spiel(gameImageName: "decay", name: "Decay - The Mare", categorie: "Point and Click", price: 6.99, teaserImageName: "minecraftheader")
+        let game3: Spiel = Spiel(gameImageName: "indianajones", name: "Indiana Jones and ...", categorie: "Point and Click", price: 4.99, teaserImageName: "minecraftheader")
+        let game4: Spiel = Spiel(gameImageName: "loom", name: "Loom", categorie: "Point and Click", price: 5.99, teaserImageName: "minecraftheader")
+        let game5: Spiel = Spiel(gameImageName: "maniacmanson", name: "Maniac Manson", categorie: "Point and Click", price: 999.99, teaserImageName: "minecraftheader")
+        let game6: Spiel = Spiel(gameImageName: "randalsmonday", name: "Randals's Monday", categorie: "Point and Click", price: 7.99, teaserImageName: "minecraftheader")
+        let game7: Spiel = Spiel(gameImageName: "samandmax", name: "Sam & Max: Hit the Road", categorie: "Point and Click", price: 29.99, teaserImageName: "minecraftheader")
+        let game8: Spiel = Spiel(gameImageName: "secretofmonkeyisland", name: "Secret of Monkey Island", categorie: "Point and Click", price: 9.99, teaserImageName: "minecraftheader")
+        let game9: Spiel = Spiel(gameImageName: "zakmckracken", name: "Zak McKracken", categorie: "Point and Click", price: 249.99, teaserImageName: "minecraftheader")
         games.append(game1)
         games.append(game2)
         games.append(game3)
@@ -71,15 +71,15 @@ class ArticleViewController: UIViewController {
     }
     
     func createGameDataRpg(){//TESTMETHODE
-        let game1: Spiel = Spiel(gameImageName: "darksouls3", name: "Darksouls 3", categorie: "Rpg", price: 29.99)
-        let game2: Spiel = Spiel(gameImageName: "deusex", name: "Deus Ex: Mankind Divided", categorie: "Rpg", price: 24.99)
-        let game3: Spiel = Spiel(gameImageName: "dragonage", name: "Dragon Age: Inquisition", categorie: "Rpg", price: 9.99)
-        let game4: Spiel = Spiel(gameImageName: "fallout4", name: "Fallout 4", categorie: "Rpg", price: 19.99)
-        let game5: Spiel = Spiel(gameImageName: "finalfantasy15", name: "Final Fantasy 15", categorie: "Rpg", price: 19.99)
-        let game6: Spiel = Spiel(gameImageName: "skyforge", name: "Skyforge", categorie: "Rpg", price: 9.99)
-        let game7: Spiel = Spiel(gameImageName: "skyrim", name: "The Elderscrolls: Skyrim", categorie: "Rpg", price: 9.99)
-        let game8: Spiel = Spiel(gameImageName: "thewitcher3", name: "The Witcher 3: Wild Hunt", categorie: "Rpg", price: 14.99)
-        let game9: Spiel = Spiel(gameImageName: "worldofwarcraft", name: "World of Warcraft", categorie: "Rpg", price: 9.99)
+        let game1: Spiel = Spiel(gameImageName: "darksouls3", name: "Darksouls 3", categorie: "Rpg", price: 29.99, teaserImageName: "minecraftheader")
+        let game2: Spiel = Spiel(gameImageName: "deusex", name: "Deus Ex: Mankind Divided", categorie: "Rpg", price: 24.99, teaserImageName: "minecraftheader")
+        let game3: Spiel = Spiel(gameImageName: "dragonage", name: "Dragon Age: Inquisition", categorie: "Rpg", price: 9.99, teaserImageName: "minecraftheader")
+        let game4: Spiel = Spiel(gameImageName: "fallout4", name: "Fallout 4", categorie: "Rpg", price: 19.99, teaserImageName: "minecraftheader")
+        let game5: Spiel = Spiel(gameImageName: "finalfantasy15", name: "Final Fantasy 15", categorie: "Rpg", price: 19.99, teaserImageName: "minecraftheader")
+        let game6: Spiel = Spiel(gameImageName: "skyforge", name: "Skyforge", categorie: "Rpg", price: 9.99, teaserImageName: "minecraftheader")
+        let game7: Spiel = Spiel(gameImageName: "skyrim", name: "The Elderscrolls: Skyrim", categorie: "Rpg", price: 9.99, teaserImageName: "minecraftheader")
+        let game8: Spiel = Spiel(gameImageName: "thewitcher3", name: "The Witcher 3: Wild Hunt", categorie: "Rpg", price: 14.99, teaserImageName: "minecraftheader")
+        let game9: Spiel = Spiel(gameImageName: "worldofwarcraft", name: "World of Warcraft", categorie: "Rpg", price: 9.99, teaserImageName: "minecraftheader")
         games.append(game1)
         games.append(game2)
         games.append(game3)
@@ -92,15 +92,15 @@ class ArticleViewController: UIViewController {
     }
     
     func createGameDataSports(){
-        let game1: Spiel = Spiel(gameImageName: "bloodbowl2", name: "Blood Bowl 2", categorie: "Sports", price: 11.99)
-        let game2: Spiel = Spiel(gameImageName: "fifa17", name: "Fifa 2017", categorie: "Sports", price: 29.99)
-        let game3: Spiel = Spiel(gameImageName: "madden", name: "Madden NFL", categorie: "Sports", price: 34.99)
-        let game4: Spiel = Spiel(gameImageName: "nba2k17", name: "NBA 2k17", categorie: "Sports", price: 29.99)
-        let game5: Spiel = Spiel(gameImageName: "nhl17", name: "NHL 2017", categorie: "Sports", price: 34.99)
-        let game6: Spiel = Spiel(gameImageName: "pes17", name: "Pro Evolution Soccer 2017", categorie: "Sports", price: 29.99)
-        let game7: Spiel = Spiel(gameImageName: "pga", name: "Rory McIlroy PGA Tour", categorie: "Sports", price: 34.99)
-        let game8: Spiel = Spiel(gameImageName: "steep", name: "Steep", categorie: "Sports", price: 29.99)
-        let game9: Spiel = Spiel(gameImageName: "wwe2k17", name: "WWE 2k17", categorie: "Sports", price: 29.99)
+        let game1: Spiel = Spiel(gameImageName: "bloodbowl2", name: "Blood Bowl 2", categorie: "Sports", price: 11.99, teaserImageName: "minecraftheader")
+        let game2: Spiel = Spiel(gameImageName: "fifa17", name: "Fifa 2017", categorie: "Sports", price: 29.99, teaserImageName: "minecraftheader")
+        let game3: Spiel = Spiel(gameImageName: "madden", name: "Madden NFL", categorie: "Sports", price: 34.99, teaserImageName: "minecraftheader")
+        let game4: Spiel = Spiel(gameImageName: "nba2k17", name: "NBA 2k17", categorie: "Sports", price: 29.99, teaserImageName: "minecraftheader")
+        let game5: Spiel = Spiel(gameImageName: "nhl17", name: "NHL 2017", categorie: "Sports", price: 34.99, teaserImageName: "minecraftheader")
+        let game6: Spiel = Spiel(gameImageName: "pes17", name: "Pro Evolution Soccer 2017", categorie: "Sports", price: 29.99, teaserImageName: "minecraftheader")
+        let game7: Spiel = Spiel(gameImageName: "pga", name: "Rory McIlroy PGA Tour", categorie: "Sports", price: 34.99, teaserImageName: "minecraftheader")
+        let game8: Spiel = Spiel(gameImageName: "steep", name: "Steep", categorie: "Sports", price: 29.99, teaserImageName: "minecraftheader")
+        let game9: Spiel = Spiel(gameImageName: "wwe2k17", name: "WWE 2k17", categorie: "Sports", price: 29.99, teaserImageName: "minecraftheader")
         
         games.append(game1)
         games.append(game2)
@@ -121,12 +121,22 @@ class ArticleViewController: UIViewController {
             let cell: GameCell? = sender as? GameCell
             
             if cell != nil && detailVC != nil {
-                detailVC!.headline = cell!.nameLabel!.text
-                detailVC!.contentText = cell!.priceLabel.text
+                let indexPath: IndexPath? = self.tableView.indexPath(for: cell!)
+                if indexPath != nil {
+                let spiel: Spiel = games[indexPath!.row]
+                    detailVC!.headline = cell!.nameLabel!.text
+                    detailVC!.priceText = cell!.priceLabel.text
+                    detailVC!.contentImage = spiel.gameImageName
+                    detailVC!.teaserImage = spiel.teaserImageName
+                    detailVC?.contentGame = cell!.nameLabel!.text
+                }
             }
         }
     }
 }
+
+
+    
 
 
 
--- WS1617_Mobu_MaKaMaGameshop/DetailViewController.swift
+++ WS1617_Mobu_MaKaMaGameshop/DetailViewController.swift
@@ -8,30 +8,134 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
 
-    var contentText: String?
+    var priceText: String?
+    var premiumText: String?
+    var deluxeText: String?
     var headline: String?
+    var teaserImage: String?
+    var contentImage: String?
+    var contentGame: String?
     
-    @IBOutlet weak var contentLabel: UILabel!
+ 
+    
+    
+    @IBOutlet weak var contentGameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var teaserImageView: UIImageView!
+    @IBOutlet weak var contentImageView: UIImageView!
+    @IBOutlet weak var editionPickerView: UIPickerView!
+    @IBOutlet weak var editionLabel: UILabel!
+    
+    
+    var edition = ["Standard", "Premium", "Deluxe"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if contentText != nil {
-            self.contentLabel.text = contentText!
+        if priceText != nil {
+            self.priceLabel.text = priceText!
         }
         
         if headline != nil {
             self.title = headline!
         }
+
+        if self.contentImage != nil {
+            self.contentImageView.image = UIImage(named: contentImage!)
+        }
         
-        // Do any additional setup after loading the view.
+        if teaserImage != nil{
+            self.teaserImageView.image = UIImage(named: teaserImage!)
+        }
+        
+        if contentGame != nil {
+            self.contentGameLabel.text = contentGame!
+        }
+        editionPickerView.delegate = self
+        editionPickerView.dataSource = self
+        
+        premiumText = priceText
+        deluxeText = priceText
+        
+        
+        
+        
     }
+    
+   /* (x: String) -> String{
+        var toFloat = (x as NSString).floatValue + 10.0
+        self.premiumText = "\(toFloat)"
+        
+        return x
+        
+    }*/
+    
+    
+    func priceLabelToFloatPremium(){// Originalpreis nehmen und 10 Euro Zuschlag für Premiumversion
+        let premiumFloat = Float(priceText!)  //price Text von String in Float
+        let sum = premiumFloat! + 10.0  // addieren
+        self.premiumText = "\(sum)"     // zurück in String und in premiumText schreiben
+    }
+    
+    func priceLabelToFloatDeluxe(){// Originalpreis nehmen und 20 Euro Zuschlag für Deluxeversion
+        let deluxeFloat = Float(priceText!)
+        let sum = deluxeFloat! + 20.0
+        self.deluxeText = "\(sum)"
+    }
+    
+    
+    //--------------PICKERVIEW------------------
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return edition.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return edition[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.editionLabel.text = edition[row]
+        
+        if edition[row] == "Standard"{
+            self.priceLabel.text = priceText!
+        }
+        if edition[row] == "Premium"{
+            priceLabelToFloatPremium()
+            self.priceLabel.text = premiumText!
+        }
+        if edition[row] == "Deluxe"{
+            priceLabelToFloatDeluxe()
+            self.priceLabel.text = deluxeText!
+        }
+    }
+    
+   /* func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == self.editionTextField{
+            self.editionPickerView.isHidden = false
+        }
+    }*/
+    
+    //---------------PICKERVIEW ENDE-----------------
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    /*func setTeaserImage(){
+        switch headline! {
+        case "Loom":
+            self.teaserImageView.image = #imageLiteral(resourceName: "minecraftheader")
+        default:
+            self.teaserImageView.image = #imageLiteral(resourceName: "minecraftheader")
+        }
+    }*/
     
     
     /*
@@ -43,6 +147,8 @@ class DetailViewController: UIViewController {
      // Pass the selected object to the new view controller.
      }
      */
-
-
 }
+
+
+
+
